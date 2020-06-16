e867c73dadd15b30acde2e591295f7e2e794e6f2
Sergey Kopeykin <nubobap2015@yandex.ru>
2020-06-16 19:25:20 +0300

    задача 9 7 ап

--- Урок 3.Практическое задание/task_9.py
+++ Урок 3.Практическое задание/task_9.py
@@ -11,3 +11,24 @@
 [13, 12, 7, 15] минимальные значения по столбцам
 Максимальное среди них = 15
 """
+import random
+
+
+def create_m(x, y):
+    return [[random.randint(10, 100) for ii in range(x)] for i in range(y)]
+
+
+def trans_m(m):
+    tmp = [[] for i in m[0]]
+    for i in m:
+        for idx2, ii in enumerate(i):
+            tmp[idx2].append(ii)
+    return tmp
+
+
+m = create_m(5, 3)
+mm = trans_m(m)
+print(m)
+print(mm)
+print('Максимальный элемент среди минимальных элементов столбцов матрицы: ',
+      max([ii for ii in [min(i) for i in mm]]))
