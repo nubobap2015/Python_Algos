be7621d8c8fc4aed196ed096a51ab366d1d46343
Sergey Kopeykin <nubobap2015@yandex.ru>
2020-07-03 18:52:28 +0300

    Обе задачи

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -16,3 +16,15 @@
 
 Итог: 6 подстрок
 """
+import hashlib
+
+
+def doit(my_string):
+    my_set = set()
+    for start in range(len(my_string)):
+        for finish in range(start + 1, 1 + len(my_string)):
+            my_set.add(hashlib.sha3_512(my_string[start:finish].encode('utf-8')).hexdigest())
+    return len(my_set) - 1
+
+
+print('Кол-во уникальных подстрок: ', doit('мама мыла раму'))
